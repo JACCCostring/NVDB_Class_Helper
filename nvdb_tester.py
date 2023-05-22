1eb1e58069f36abb57f6795d8f6065457102513a
JACC <93591202+JACCCostring@users.noreply.github.com>
2023-05-22 12:53:15 +0200

    Update nvdb_tester.py
    test body must be added just to show as an example

--- nvdb_tester.py
+++ nvdb_tester.py
@@ -1,31 +1,6 @@
 from nvdbWrapper import NVDBWrapperClass
-from nvdbWrapper import EspecificObjectTasks
-
-def write_to_file(filename, data):
-    with open(filename, 'w') as file:
-        file.write(data)
 
 def main():
-    raw = NVDBWrapperClass.rawObject(581)
-    # data = EspecificObjectTasks.findRelation(EspecificObjectTasks.constructEndPoint(67, 89201351), 'Ventilasjonsanlegg')
-    # vegRef = EspecificObjectTasks.findVegReferanse(67, 89201351)
-    # d = EspecificObjectTasks.getMeta(581, 89201350)
-    # print(data)
-    more = raw.nesteForekomst()
-
-    while more:
-        for object in raw:
-            name = EspecificObjectTasks.getMeta(object)['navn']
-            id = EspecificObjectTasks.getMeta(object)['id']
-
-            relation = object['relasjoner']
-            barn = relation['barn']
-
-            for object in barn:
-                for vegObj in object['vegobjekter']:
-                    data = EspecificObjectTasks.findRelation(EspecificObjectTasks.constructEndPoint(67, int(vegObj)), parentObjectName=name, objectType=id)
-                    print(data)
-
-        more = raw.nesteForekomst()
+#code here
    
 main()
