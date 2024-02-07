f378a576b72fcbb186e164d202f0187f2a4d9450
JACC <93591202+JACCCostring@users.noreply.github.com>
2024-02-07 12:52:39 +0100

    Update abstractPoster.py
    ABC and abstractmethod package imported, to make abstract class

--- abstractPoster.py
+++ abstractPoster.py
@@ -1,12 +1,15 @@
-import xml.etree.ElementTree as ET 
+import xml.etree.ElementTree as ET
+from abc import ABC, abstractmethod
 
-class AbstractPoster:
+class AbstractPoster(ABC):
     def __init__(self, modified_data):
         #self.token = token
         self.modified_data = modified_data
-
+        
+    @abstractmethod
     def prepare_post(self):
         pass
-    
+        
+    @abstractmethod
     def formXMLRequest(self, egenskaper_list):
         pass
